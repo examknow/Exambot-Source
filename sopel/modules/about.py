41aacfe8541438ba8f20974c7049861d487dba25
examknow <54456226+examknow@users.noreply.github.com>
2019-11-06 22:06:08 -0500

    Created module

---
+++ sopel/modules/about.py
@@ -0,0 +1,16 @@
+from sopel import module
+
+@module.commands('about')
+@module.example('.about')
+@module.example('.about help')
+def about(bot, trigger):
+    argument = trigger.group(2)
+    if argument == help
+      bot.say("Examknow: I just recived a help request from " + trigger.nick + " in " + trigger.sender)
+      bot.say("Help can be by saying .help in any ExamBot channel or ask an admin in #ExamBot")
+    else
+      bot.say("I am running ExamBot version 3.0.1 . If I am acting unstable please alert Examknow, my operator. For help say .about help. For a link to my source code on GitHub say .source . Thank you for choosing ExamBot.")
+
+@module.commands('source', 'repo')
+def source(bot, trigger):
+  bot.say("My source code is avalible at https://github.com/Exambot-Source")
